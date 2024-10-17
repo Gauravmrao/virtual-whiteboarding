911325b658150f1e7035f346e9748e13083e062d
Gaurav Rao <69742533+Gauravmrao@users.noreply.github.com>
2024-10-17 13:52:58 -0400

    added color selection mode vs drawing mode

--- Painter.py
+++ Painter.py
@@ -37,35 +37,84 @@ while True:
 
     # control flow for the fingers
     if len(landmarks) > 0:
+
+        # finger tips
         indexX = landmarks[8][1]
         indexY = landmarks[8][2]
         middleX = landmarks[12][1]
         middleY = landmarks[12][2]
-        cv2.circle(img, (indexX, indexY), markerSize, currentColor, cv2.FILLED)
-        cv2.putText(img, f'{str(int(indexX))}', (indexX, indexY), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
+        #cv2.putText(img, f'{str(int(indexX))}', (indexX, indexY), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
+
+        # knuckles
+        indKnuckleX = landmarks[6][1]
+        indKnuckleY = landmarks[6][2]
+        midKnuckleX = landmarks[10][1]
+        midKnuckleY = landmarks[10][2]
+
+
+        """ 
+        calculate the cross product of the index and middle fingertips
+        this will allow us to check if both fingers are raised, or in this case,
+        if both fingertips are on the same side of the vector created by the line
+        segment between the two knuckles. This can be used for changing the modes 
+        between drawing and color selection.
+        """
+
+        # Calculate the cross product of two 2D vectors v1 and v2
+        def cross_product(v1, v2):
+            return v1[0] * v2[1] - v1[1] * v2[0]
+
+        # Check if points P and Q are on the same side of the line created by the segment AB.
+        def sameSide(A, B, P, Q):
+            # Vector AB
+            AB = (B[0] - A[0], B[1] - A[1])
+            # Vector AP and AQ
+            AP = (P[0] - A[0], P[1] - A[1])
+            AQ = (Q[0] - A[0], Q[1] - A[1])
+            # Cross products
+            cross1 = cross_product(AB, AP)
+            cross2 = cross_product(AB, AQ)
+            # Check if the cross products have the same sign
+            return cross1 * cross2 >= 0  # Same side if the signs are the same
+
+
+        # color selection mode - selecting the color
+        if sameSide((indKnuckleX, indKnuckleY), (midKnuckleX, midKnuckleY), (indexX, indexY), (middleX, middleY)):
+            cv2.circle(img, (indexX, indexY), markerSize, currentColor, 5)
+            if indexY < 120:
+                if indexX >= 425 and indexX <= 530:
+                    currentHeader = headers[0]  # red
+                    currentColor = (0, 0, 255)
+                    markerSize = 15
+                if indexX >= 575 and indexX <= 680:
+                    currentHeader = headers[1]  # green
+                    currentColor = (0, 255, 0)
+                    markerSize = 15
+                if indexX >= 730 and indexX <= 840:
+                    currentHeader = headers[2]  # blue
+                    currentColor = (255, 0, 0)
+                    markerSize = 15
+                if indexX >= 915 and indexX <= 1020:
+                    currentHeader = headers[3]  # eraser
+                    currentColor = (255, 255, 255)
+                    markerSize = 40
+        
+        # drawing mode
+        else:
+            cv2.circle(img, (indexX, indexY), markerSize, currentColor, cv2.FILLED)
+            ...
+
+
+        
+
+
+
 
 
         distance = (((middleX - indexX) ** 2) + ((middleY - indexY) ** 2)) ** 0.5
         #cv2.putText(img, f'{str(int(distance))}', (indexX, indexY), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
 
-        # selecting the color
-        if indexY < 120:
-            if indexX >= 425 and indexX <= 530:
-                currentHeader = headers[0]  # red
-                currentColor = (0, 0, 255)
-                markerSize = 15
-            if indexX >= 575 and indexX <= 680:
-                currentHeader = headers[1]  # green
-                currentColor = (0, 255, 0)
-                markerSize = 15
-            if indexX >= 730 and indexX <= 840:
-                currentHeader = headers[2]  # blue
-                currentColor = (255, 0, 0)
-                markerSize = 15
-            if indexX >= 915 and indexX <= 1020:
-                currentHeader = headers[3]  # eraser
-                currentColor = (0, 0, 0)
-                markerSize = 40
+        
             
             
             
